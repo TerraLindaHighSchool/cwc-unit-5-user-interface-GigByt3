0c76b0e75998537d1928095fb526c9ca5b838a63
Finlay Norton-Lindsay <p3n.duke@gmail.com>
2020-03-12 14:49:39 -0700

    Everything (wrong repo)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOver;
+    public GameObject restartButton;
+    public GameObject ScoreUI;
+    public GameObject TitleUI;
+    private float spawnRate = 0.6f;
+    private int score;
+    private bool gameOn;
+    // Start is called before the first frame update
+
+    public void StartGame(int diff)
+    {
+        ScoreUI.SetActive(true);
+        TitleUI.SetActive(false);
+        restartButton.SetActive(false);
+        gameOver.gameObject.SetActive(false);
+        spawnRate *= diff;
+        gameOn = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
+        Debug.Log("CLicked/Started!");
+    }
+
+    public void UpdateScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+
+    }
+
+    public void EndGame()
+    {
+        gameOver.gameObject.SetActive(true);
+        gameOn = false;
+        restartButton.SetActive(true);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(gameOn)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    private GameManager gameManager;
+    private Rigidbody targetRB;
+    private float minSpeed = 500; private float maxSpeed = 600;
+    private float maxTorque = 20; private float xRange = 5;
+    private float ySpawnPos = 11;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        targetRB = GetComponent<Rigidbody>();
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(RandomTorque(),
+                           RandomTorque(),
+                           RandomTorque(),
+                           ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+    }
+
+    Vector3 RandomForce() { return Vector3.up; }
+    float RandomTorque() { return Random.Range(-maxTorque, maxTorque); }
+    Vector3 RandomSpawnPos() { return new Vector3(Random.Range(-xRange, xRange), ySpawnPos); }
+
+    private void OnMouseDown() {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position,
+            explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.EndGame();
+        }
+        Destroy(this.gameObject);
+        Instantiate(explosionParticle, transform.position,
+            explosionParticle.transform.rotation);
+    }
+}
